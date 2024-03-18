f22473f753a3c8ae7144fd0edef7cb0b813abd34
Takeaki23 <97432199+Takeaki23@users.noreply.github.com>
2024-03-18 21:15:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // Placeholder array of image URLs
   const images = [
     "daimyooo_Japanese_creation_Hoshino_Ai_full_body_view_detailed_b_11deb920-7c93-4e8b-913c-5488a21ba9da.png",
-    "image2.jpg",
+    "daimyooo_Japanese_creation_Hoshino_Ai_full_body_view_detailed_b_26503842-c97c-4e26-9aa3-d956c022c640.png",
     "image3.jpg",
     // Add more image URLs as needed
   ];
